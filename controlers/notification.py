ec8d5d70559fc73069b3ffecd3cb5f6f288fd765
Rodrigo Pérez <rgperez@antel.com.uy>
2019-07-19 16:10:25 -0300

    Agrego metodos a implementar

--- controlers/notification.py
+++ controlers/notification.py
@@ -4,15 +4,15 @@ from models.notification import NotificationModel
 class NotificationControler():
 
     @staticmethod
-    def get_all_notifications()  -> list:
+    def get_all_notifications() -> list:
         return NotificationModel.get_notification_list()
 
     @staticmethod
     def get_atributes_list() -> list:
-        return ["imei","ip","fecha","ecno","rssi","cellid","log_size","temp","uptime"]
+        return ["imei", "ip", "fecha", "ecno", "rssi", "cellid", "log_size", "temp", "uptime"]
 
     @staticmethod
-    def get_active_meter_list(last_time) -> list:
+    def get_active_meter_list(period_time) -> list:
         return None
 
     @staticmethod
@@ -21,4 +21,15 @@ class NotificationControler():
 
     @staticmethod
     def get_low_signal_meter_list(min_power) -> list:
+        '''Listado de medidores que tienen baja señal'''
+        return None
+
+    @staticmethod
+    def get_down_meter(period_time) -> list:
+        '''Listado de medidores que no se reportaron en cierto periodo de tiempo'''
+        return None
+
+    @staticmethod
+    def get_meter_by_error_code(period_time, error_code) -> list:
+        '''Listado de medidores que reportaron cierto error de codigo'''
         return None
